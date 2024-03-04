ef1f0bd78ae9988456d2add4424da1c035869b37
Shestakov Vladyslav <vladyslav.shestakov@cyper.ca>
2024-03-04 11:02:58 +0200

    styles

--- src/app/dashboard/components/News/News.js
+++ src/app/dashboard/components/News/News.js
@@ -6,18 +6,26 @@ import styles from "./style.module.scss"
 const News = () => {
     return (
         <section className={styles.section}>
-            <div className={styles.title_wrapper}>
-                <div className={styles.title}>
-                    News
+            <div className={styles.inner}>
+                <div className={styles.title_wrapper}>
+                    <div className={styles.title}>
+                        News
+                    </div>
+                    <a
+                        href="https://cyberpolice.gov.ua/"
+                        target="blank"
+                    >
+                        <Button
+                            className={styles.button}
+                        >
+                            Read more
+                            <img
+                                src="/icons/icon-arrow.svg"
+                                className={styles.icon}
+                            />
+                        </Button>
+                    </a>
                 </div>
-                <a
-                    href="https://cyberpolice.gov.ua/"
-                    target="blank"
-                >
-                    <Button>
-                        Read more
-                    </Button>
-                </a>
             </div>
         </section>
     )
