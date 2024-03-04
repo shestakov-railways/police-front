4b134b6d6e48ad65b44f64a7d9004540d63ecf55
Shestakov Vladyslav <vladyslav.shestakov@cyper.ca>
2024-03-05 01:23:58 +0200

    changes

--- src/app/form/components/index.js
+++ src/app/form/components/index.js
@@ -3,11 +3,13 @@ import Form from "./Form/Form"
 import FirstStep from "./FirstStep/FirstStep"
 import SecondStep from "./SecondStep/SecondStep"
 import ThirdStep from "./ThirdStep/ThirdStep"
+import NestedArray from "./NestedArray/NestedArray"
 
 export {
     Page,
     Form,
     FirstStep,
     SecondStep,
-    ThirdStep
+    ThirdStep,
+    NestedArray,
 }
