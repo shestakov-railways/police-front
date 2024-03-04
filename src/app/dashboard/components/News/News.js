b0df1cf6b87002713db3506bf01af97b25dd7549
Shestakov Vladyslav <vladyslav.shestakov@cyper.ca>
2024-03-04 11:20:36 +0200

    news

--- src/app/dashboard/components/News/News.js
+++ src/app/dashboard/components/News/News.js
@@ -26,6 +26,68 @@ const News = () => {
                         </Button>
                     </a>
                 </div>
+
+                <div className={styles.news_list}>
+                    <a
+                        href="https://cyberpolice.gov.ua/news/biznes-z-miljonnym-oborotom-grupa-narkodilkiv-postane-pered-sudom-na-lvivshhyni-4123/"
+                        target="blank"
+                        className={styles.news}
+                    >
+                        <div className={styles.news_title}>
+                            Cybersecurity alert issued
+                        </div>
+                        <div className={styles.news_text}>
+                            Stay ahead of potential threats and protect your data with our comprehensive incident prevention strategies. Learn how to safeguard your information and enhance your online security today.
+                        </div>
+                        <div className={styles.image_wrapper}>
+                            <img
+                                src="/news/image-1.png"
+                                alt=""
+                                className={styles.image}
+                            />
+                        </div>
+                    </a>
+
+                    <a
+                        href="https://cyberpolice.gov.ua/news/biznes-z-miljonnym-oborotom-grupa-narkodilkiv-postane-pered-sudom-na-lvivshhyni-4123/"
+                        target="blank"
+                        className={styles.news}
+                    >
+                        <div className={styles.news_title}>
+                            Cybersecurity alert issued
+                        </div>
+                        <div className={styles.news_text}>
+                            Stay ahead of potential threats and protect your data with our comprehensive incident prevention strategies. Learn how to safeguard your information and enhance your online security today.
+                        </div>
+                        <div className={styles.image_wrapper}>
+                            <img
+                                src="/news/image-1.png"
+                                alt=""
+                                className={styles.image}
+                            />
+                        </div>
+                    </a>
+
+                    <a
+                        href="https://cyberpolice.gov.ua/news/biznes-z-miljonnym-oborotom-grupa-narkodilkiv-postane-pered-sudom-na-lvivshhyni-4123/"
+                        target="blank"
+                        className={styles.news}
+                    >
+                        <div className={styles.news_title}>
+                            Cybersecurity alert issued
+                        </div>
+                        <div className={styles.news_text}>
+                            Stay ahead of potential threats and protect your data with our comprehensive incident prevention strategies. Learn how to safeguard your information and enhance your online security today.
+                        </div>
+                        <div className={styles.image_wrapper}>
+                            <img
+                                src="/news/image-1.png"
+                                alt=""
+                                className={styles.image}
+                            />
+                        </div>
+                    </a>
+                </div>
             </div>
         </section>
     )
